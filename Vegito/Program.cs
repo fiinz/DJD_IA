e09ab818a6f50ad186562202150144a2ba497644
Jaime Fins <jaime.fins@gmail.com>
2019-09-28 17:23:41 +0100

    Resolução de exercicio #vegito

---
+++ Vegito/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vegito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
