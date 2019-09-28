4f9e475ff13e57480b241a4db34570e740597e1a
Jaime Fins <jaime.fins@gmail.com>
2019-09-28 18:40:15 +0100

    One Added

--- LetTheGamesBegin/Program.cs
+++ LetTheGamesBegin/Program.cs
@@ -7,6 +7,7 @@ namespace LetTheGamesBegin
         static void Main(string[] args)  
         {
             Console.WriteLine("teste3");
+            Console.WriteLine("teste3");
             Console.WriteLine("Hello World!");
         }
     }
