9586aaea98481a595af5f2f7fa8be6949453be09
fiinz <jaime.fins@gmail.com>
2019-09-30 15:23:48 +0100

    Update Noob.cs

--- Noob.cs
+++ Noob.cs
@@ -1,2 +1,22 @@
-Create an algorithm that defines and stores Player's nickname and it says "You’re such a Noob #Fiinz!
- It's always good to have newbies around here!". Being #Fiinz, in this case,  the selected, name of the Player
+using System;
+/* Jaime Fins 2019*/
+
+namespace LetTheGamesBegin
+{
+    // #LetTheGamesBegin
+    /// <summary>Create an algorithm that defines and stores Player's nickname and it says "You’re such a Noob #Fiinz!
+   // It's always good to have newbies around here!". Being #Fiinz, in this case,  the selected, name of the Player
+    // <see cref="System.Console.WriteLine(System.String)"/> for information about output statements
+    /// </summary>
+    
+    class Program
+    {
+        static void Main(string[] args)  
+        {
+            Console.WriteLine("Game Started");
+            Console.WriteLine("Game Playing");
+            Console.WriteLine("Game Over");
+         
+        }
+    }
+}
