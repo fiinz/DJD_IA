1de3ad035ba1124b8a0b77757a1eeb1d57293ab5
fiinz <jaime.fins@gmail.com>
2019-09-30 15:22:23 +0100

    Create Noob.cs

---
+++ Noob.cs
@@ -0,0 +1,2 @@
+Create an algorithm that defines and stores Player's nickname and it says "You’re such a Noob #Fiinz!
+ It's always good to have newbies around here!". Being #Fiinz, in this case,  the selected, name of the Player
