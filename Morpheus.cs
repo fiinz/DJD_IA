30c7648286c01a4bb2cb591cb65d02448804a1e8
Jaime <jaime.fins@gmail.com>
2020-01-04 17:56:38 +0000

    Added Matrixs

---
+++ Morpheus.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Morpheus
+{
+    class Program
+    {
+        /// <summary>
+        /// Morpheys
+        /// Create and Fill two multidimensional arrays, A( 4, 6)  and B (4, 6) and create:
+        // a) a matrix S which is the sum of A and B. (A+ B)
+        // b) a matrix D that is the difference from A and B. (A - B).
+        // Write matrices S and D after all calculations are completed
+        /// </summary>
+        /// <param name="args"></param>
+
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            int cols = 6;
+
+            int[,] matrixA = new int[rows, cols];
+            int[,] matrixB = new int[rows, cols];
+
+            ReadMatrix(matrixA);
+            ReadMatrix(matrixB);
+
+
+            Console.WriteLine(" A Matrix");
+            PrintMatrix(matrixA);
+            Console.WriteLine("");
+
+            Console.WriteLine(" B Matrix");
+            PrintMatrix(matrixB);
+            Console.WriteLine("");
+
+            Console.WriteLine(" A+B Matrix");
+            PrintMatrix(SumMatrixs(matrixA,matrixB));
+            Console.WriteLine("");
+
+            Console.WriteLine(" A-B Matrix");
+            PrintMatrix(SubtractMatrixs(matrixA, matrixB));
+            Console.WriteLine("");
+
+
+
+
+        }
+        static int[,] SumMatrixs(int[,] mA, int[,] mB)
+        {
+            int[,] matrix = new int[mA.GetLength(0), mA.GetLength(1)];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = mA[row, col] + mB[row, col];
+                }
+            }
+
+           return matrix;
+
+        }
+        static int[,] SubtractMatrixs(int[,] mA, int[,] mB)
+        {
+            int[,] matrix = new int[mA.GetLength(0), mA.GetLength(1)];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = mA[row, col] - mB[row, col];
+                }
+            }
+
+            return matrix;
+
+        }
+        static void PrintMatrix(int[,] m)
+        {
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                for (int col = 0; col < m.GetLength(1); col++)
+                {
+
+                    Console.Write(m[row, col]);
+                    Console.Write("\t");
+                }
+                Console.Write("\n");
+            }
+
+        }
+        static void ReadMatrix(int[,] m)
+        {
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                for (int col = 0; col < m.GetLength(0); col++)
+                {
+
+                    m[row, col] = r.Next(1, 10);
+                }
+            }
+        }
+    }
+}
---
+++ Neo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Neo
+{
+
+    class Program
+    {
+        /// <summary>
+        // Read an M 5 x 5 matrix, calculate and write the following sums:
+        // a) from line 3 of M
+        // b) from column 2 of M
+        // c) the main diagonal
+        //d) the secondary diagonal
+        // e) of all matrix elements
+
+        /// </summary>
+        /// <param name="args"></param>
+        ///
+
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            int rows = 5;
+            int cols = 5;
+
+            int[,] matrix = new int[rows, cols];
+            ReadMatrix(matrix);
+            PrintMatrix(matrix);
+            Console.WriteLine("");
+            Console.WriteLine("Sum Row 3 : " + SumRow(matrix, 3));
+            Console.WriteLine("Sum Col 2 : " + SumCol(matrix, 2));
+            Console.WriteLine("Sum Diagonal : " + SumMainDiagonal(matrix));
+            Console.WriteLine("Sum Secondary Diagonal : " + SumSecondaryDiagonal(matrix));
+
+
+
+        }
+        static void PrintMatrix(int [,]m)
+        {
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                for (int col = 0; col < m.GetLength(1); col++)
+                {
+
+                    Console.Write(m[row, col]);
+                  //  Console.Write("\t");
+                }
+                Console.Write("\n");
+            }
+
+        }
+        static void ReadMatrix(int[,] m)
+        {
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                for (int col = 0; col < m.GetLength(0); col++)
+                {
+
+                    m[row, col] = r.Next(1, 10);
+                }
+            }
+        }
+        static int SumRow(int[,] m, int row)
+        {
+            //row is the index
+
+            int sum = 0;
+            for (int col = 0; col < m.GetLength(1); col++)
+            {
+                sum += m[row, col];
+
+            }
+            return sum;
+        }
+
+        static int SumCol(int[,] m, int col)
+        {
+            //col is the index
+            int sum = 0;
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                sum += m[row, col];
+
+            }
+            return sum;
+        }
+
+        static int SumMainDiagonal(int[,] m)
+        {
+            int sum = 0;
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                sum += m[row, row];
+
+            }
+            return sum;
+        }
+
+        static int SumSecondaryDiagonal(int[,] m)
+        {
+            int sum = 0;
+            for (int row = 0; row < m.GetLength(0); row++)
+            {
+                sum += m[row, m.GetLength(0)-1-row];
+
+            }
+            return sum;
+        }
+    }
+}
